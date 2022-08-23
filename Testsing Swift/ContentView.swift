f7dfb7a2ba3c58c9e15061e16ae20ccd251519b9
refractored <fatboy11905@gmail.com>
2022-08-23 10:35:30 -0500

    Initial Commit

---
+++ Testsing Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Testsing Swift
+//
+//  Created by David M. Galvan on 8/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testsing Swift/Testsing_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testsing_SwiftApp.swift
+//  Testsing Swift
+//
+//  Created by David M. Galvan on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Testsing_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
